18a59acbeff064d0d247e83730054f2f4f4b3dde
apellicerep <arturomailbcn@gmail.com>
2018-09-12 17:56:24 +0100

    Array of Quote Objects

--- js/script.js
+++ js/script.js
@@ -1,6 +1,40 @@
 // FSJS - Random Quote Generator
 
-// Create the array of quote objects and name it quotes
+// Array of Quotes Objects.
+
+var quotes =[
+  {
+    quote:"In this world nothing can be said to be certain, except death and taxes.",
+    source:"Benjamin Franklin",
+    tag:"Finance"
+  },
+  {
+    quote:"Love all, trust a few, do wrong to none.",
+    source:"William Shakespeare",
+    tag:"Love"
+
+  }
+  {
+    quote:"Life is 20% what happens to you and 90% how you react it.",
+    source:"Charles R. Swindoll",
+    tag:"Motivational"
+  }
+  {
+    quote:"A dream doesn’t become reality through magic; it takes sweat, determination and hard work.",
+    source:"Colin Powell",
+    tag:"Work"
+  }
+  {
+    quote:"Luck is great, but most of life is hard work.",
+    source:"Iain Duncan Smith",
+    tag:"Work"
+  }
+  {
+    quote:"Ideas pull the trigger, but instinct loads the gun.",
+    source:"Don Marquis",
+    tag:"Work"
+  }
+]
 
 
 
