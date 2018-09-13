b5fad6eb0fd9609d252a4da8b43c1b504dcbbbf7
apellicerep <arturomailbcn@gmail.com>
2018-09-13 08:54:47 +0100

    setIntervals() and changColor()
    backgroundColor and Quotes changes randomly every interval.

--- js/script.js
+++ js/script.js
@@ -1,4 +1,4 @@
-// FSJS - Random Quote Generator
+// Random Quote Generator - Apellicer
 
 // Array of Quotes Objects.
 
@@ -36,13 +36,16 @@ var quotes =[
   }
 ]
 
+var intervalColor;
+var intervalQuote;
+
 
 // it gets a Random Object from the Array and return it.
 
 function getRandomQuote(array){
   var quoteToSend; // Quote Object that we will return.
-  topRandomNumber = quotes.length; //I get length of the Array to be my top in my random function.
-  positionQuote = Math.floor(Math.random()*(topRandomNumber));
+  var topRandomNumber = quotes.length; //I get length of the Array to be my top in my random function.
+  var positionQuote = Math.floor(Math.random()*(topRandomNumber));
   //method that returns random number between 0 and (topRandomNumber), I will use as a index of my Array of Objects.
   quoteToSend = quotes[positionQuote];
   return quoteToSend;
@@ -68,6 +71,30 @@ function printQuote(){
   }html +="</p>";
   document.getElementById('quote-box').innerHTML = html; //print hmtl.
 }
+
+//  BackgroundColor and Quotes changes randomly every interval.
+
+function setIntervals(){
+
+  intervalColor = setInterval(changeColor,6000); //set the interval 6s and call function changeColor.
+  intervalQuote = setInterval(printQuote,6000); //set the interval 6s and call function printQuote
+}
+
+//  Change style to a random background color.
+
+function changeColor(){
+  var randomColor1 = Math.floor(Math.random()*(200)); //get random number to create the rgb color.
+  var randomColor2 = Math.floor(Math.random()*(200));
+  var randomColor3 = Math.floor(Math.random()*(200));
+  var colorRgb = [randomColor1,randomColor2,randomColor3];
+  var stringColors = colorRgb.join(',');
+  var stringRgb = "rgb("+stringColors+")"; //create a string of colors bcause doesn't accept this format rgb() as a function
+                                          //must be a String.
+
+  document.body.style.backgroundColor = stringRgb; //select tag body and change the style with a String.
+}
+
+setIntervals(); //call function.
 
 
 // This event listener will respond to "Show another quote" button clicks
